61660febbbe3ed40b21d9dfe340ee53ace21b721
kazu7719 <kazuki.eripon7719@icloud.com>
2026-01-27 16:10:13 +0900

    バリデーションの更新

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,9 @@
 class Article < ApplicationRecord
   enum status: { draft: 0, published: 1, archived: 2 }
 
-  validates :title, presence: { message: 'を入力してください' },
-                    length: { maximum: 255, message: 'は255文字以内で入力してください' }
-  validates :body, presence: { message: 'を入力してください' }
+  validates :title, presence: { message: 'タイトルを入力してください' },
+                    length: { minimum: 1, maximum: 100, message: 'タイトルは100文字以内で入力してください' }
+  validates :body, presence: { message: '本文を入力してください' }
   validates :status, presence: { message: 'を入力してください' }
 
   validate :validate_published_at_presence
