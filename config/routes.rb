59992f8a407a71cdfb290c932953964f2ee03dc8
kazu7719 <kazuki.eripon7719@icloud.com>
2026-01-28 10:36:52 +0900

    commentコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:index, :show, :create, :update, :destroy]
+  resources :articles, only: [:index, :show, :create, :update, :destroy] do
+    resources :comments, only: [:index, :create, :destroy]
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
