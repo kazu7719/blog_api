e3b20a07d48932f4812932018d1953b3534e1441
kazu7719 <kazuki.eripon7719@icloud.com>
2026-01-29 09:31:25 +0900

    articleモデルのテストコード作成

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -25,25 +25,25 @@ RSpec.describe Article, type: :model do
       it 'titleが空の場合、無効である' do
         article = build(:article, title: '')
         expect(article).not_to be_valid
-        expect(article.errors[:title]).to include('を入力してください')
+        expect(article.errors[:title]).to include('タイトルを入力してください')
       end
 
       it 'titleがnilの場合、無効である' do
         article = build(:article, title: nil)
         expect(article).not_to be_valid
-        expect(article.errors[:title]).to include('を入力してください')
+        expect(article.errors[:title]).to include('タイトルを入力してください')
       end
 
       it 'bodyが空の場合、無効である' do
         article = build(:article, body: '')
         expect(article).not_to be_valid
-        expect(article.errors[:body]).to include('を入力してください')
+        expect(article.errors[:body]).to include('本文を入力してください')
       end
 
       it 'bodyがnilの場合、無効である' do
         article = build(:article, body: nil)
         expect(article).not_to be_valid
-        expect(article.errors[:body]).to include('を入力してください')
+        expect(article.errors[:body]).to include('本文を入力してください')
       end
 
       it 'statusがpublishedでpublished_atがない場合、無効である' do
@@ -55,15 +55,15 @@ RSpec.describe Article, type: :model do
 
     # 境界値
     context '境界値' do
-      it 'titleが255文字の場合、有効である' do
-        article = build(:article, title: 'a' * 255)
+      it 'titleが100文字の場合、有効である' do
+        article = build(:article, title: 'a' * 100)
         expect(article).to be_valid
       end
 
-      it 'titleが256文字の場合、無効である' do
-        article = build(:article, title: 'a' * 256)
+      it 'titleが101文字の場合、無効である' do
+        article = build(:article, title: 'a' * 101)
         expect(article).not_to be_valid
-        expect(article.errors[:title]).to include('は255文字以内で入力してください')
+        expect(article.errors[:title]).to include('タイトルは100文字以内で入力してください')
       end
     end
   end
@@ -71,6 +71,51 @@ RSpec.describe Article, type: :model do
   describe 'enum' do
     it 'statusにdraft, published, archivedが定義されていること' do
       expect(Article.statuses).to eq({ 'draft' => 0, 'published' => 1, 'archived' => 2 })
+    end
+
+    it 'デフォルトのstatusはdraftである' do
+      article = Article.new
+      expect(article.status).to eq('draft')
+    end
+
+    it 'statusをpublishedに変更できる' do
+      article = create(:article)
+      article.update(status: :published, published_at: Time.current)
+      expect(article.status_published?).to be true
+    end
+
+    it 'statusをarchivedに変更できる' do
+      article = create(:article)
+      article.update(status: :archived)
+      expect(article.status_archived?).to be true
+    end
+
+    it 'status_draft?メソッドが正しく動作する' do
+      article = build(:article)
+      expect(article.status_draft?).to be true
+    end
+
+    it 'status_published?メソッドが正しく動作する' do
+      article = build(:article, :published)
+      expect(article.status_published?).to be true
+    end
+
+    it 'status_archived?メソッドが正しく動作する' do
+      article = build(:article, :archived)
+      expect(article.status_archived?).to be true
+    end
+  end
+
+  describe 'アソシエーション' do
+    it 'commentsを持つ' do
+      association = described_class.reflect_on_association(:comments)
+      expect(association.macro).to eq :has_many
+    end
+
+    it '記事を削除すると関連するコメントも削除される' do
+      article = create(:article)
+      create(:comment, article: article)
+      expect { article.destroy }.to change { Comment.count }.by(-1)
     end
   end
 end
